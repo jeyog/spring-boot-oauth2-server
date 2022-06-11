d51ab7f29658a7e63f267f40db141c63b44d93d6
jeyog <choijy8816@gmail.com>
2022-06-12 00:23:15 +0900

    Remove JdbcOauth2 configuration

--- src/main/kotlin/com/jeyog/oauth2/config/AuthorizationServerConfig.kt
+++ src/main/kotlin/com/jeyog/oauth2/config/AuthorizationServerConfig.kt
@@ -10,30 +10,14 @@ import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.core.Ordered
 import org.springframework.core.annotation.Order
-import org.springframework.jdbc.core.JdbcTemplate
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.OAuth2AuthorizationServerConfiguration
-import org.springframework.security.crypto.password.PasswordEncoder
-import org.springframework.security.oauth2.core.AuthorizationGrantType
-import org.springframework.security.oauth2.core.ClientAuthenticationMethod
-import org.springframework.security.oauth2.core.oidc.OidcScopes
-import org.springframework.security.oauth2.server.authorization.JdbcOAuth2AuthorizationConsentService
-import org.springframework.security.oauth2.server.authorization.JdbcOAuth2AuthorizationService
-import org.springframework.security.oauth2.server.authorization.OAuth2AuthorizationConsentService
-import org.springframework.security.oauth2.server.authorization.OAuth2AuthorizationService
-import org.springframework.security.oauth2.server.authorization.client.JdbcRegisteredClientRepository
-import org.springframework.security.oauth2.server.authorization.client.RegisteredClient
-import org.springframework.security.oauth2.server.authorization.client.RegisteredClientRepository
-import org.springframework.security.oauth2.server.authorization.config.ClientSettings
 import org.springframework.security.oauth2.server.authorization.config.ProviderSettings
 import org.springframework.security.web.SecurityFilterChain
 import org.springframework.security.web.authentication.LoginUrlAuthenticationEntryPoint
-import java.util.*
 
 @Configuration
-class AuthorizationServerConfig(
-        private val passwordEncoder: PasswordEncoder
-) {
+class AuthorizationServerConfig {
     @Bean
     @Order(Ordered.HIGHEST_PRECEDENCE)
     @Throws(Exception::class)
@@ -45,38 +29,6 @@ class AuthorizationServerConfig(
                     exceptions.authenticationEntryPoint(LoginUrlAuthenticationEntryPoint("/login"))
                 }
         return http.build()
-    }
-
-    @Bean
-    fun registeredClientRepository(jdbcTemplate: JdbcTemplate?): RegisteredClientRepository {
-        val registeredClient = RegisteredClient.withId(UUID.randomUUID().toString())
-                .clientId("messaging-client")
-                .clientSecret(passwordEncoder.encode("secret"))
-                .clientAuthenticationMethod(ClientAuthenticationMethod.CLIENT_SECRET_BASIC)
-                .authorizationGrantType(AuthorizationGrantType.AUTHORIZATION_CODE)
-                .authorizationGrantType(AuthorizationGrantType.REFRESH_TOKEN)
-                .redirectUri("http://127.0.0.1:8080/login/oauth2/code/messaging-client-oidc")
-                .redirectUri("http://127.0.0.1:8080/authorized")
-                .scope(OidcScopes.OPENID)
-                .scope("message.read")
-                .scope("message.write")
-                .clientSettings(ClientSettings.builder().requireAuthorizationConsent(true).build())
-                .build()
-
-        // Save registered client in db as if in-memory
-        val registeredClientRepository = JdbcRegisteredClientRepository(jdbcTemplate)
-        registeredClientRepository.save(registeredClient)
-        return registeredClientRepository
-    }
-
-    @Bean
-    fun authorizationService(jdbcTemplate: JdbcTemplate?, registeredClientRepository: RegisteredClientRepository?): OAuth2AuthorizationService {
-        return JdbcOAuth2AuthorizationService(jdbcTemplate, registeredClientRepository)
-    }
-
-    @Bean
-    fun authorizationConsentService(jdbcTemplate: JdbcTemplate?, registeredClientRepository: RegisteredClientRepository?): OAuth2AuthorizationConsentService {
-        return JdbcOAuth2AuthorizationConsentService(jdbcTemplate, registeredClientRepository)
     }
 
     @Bean
