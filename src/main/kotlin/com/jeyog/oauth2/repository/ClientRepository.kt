afeba1b6bbaa11112ec8a093acfe30044ac16018
jeyog <choijy8816@gmail.com>
2022-06-11 22:58:39 +0900

    Change wrong import to correctly

--- src/main/kotlin/com/jeyog/oauth2/repository/ClientRepository.kt
+++ src/main/kotlin/com/jeyog/oauth2/repository/ClientRepository.kt
@@ -1,6 +1,6 @@
 package com.jeyog.oauth2.repository
 
-import ch.qos.logback.core.net.server.Client
+import com.jeyog.oauth2.entity.Client
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.stereotype.Repository
 
